351bf96586ed8155ea492b30a4f7048f9c31fad4
Balwant <balwantsingh3186@gmail.com>
2024-02-20 22:57:56 +0530

    react-split package added

---
+++ src/components/Workspace/Workspace.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ProblemDescription from "./ProblemDescription/ProblemDescription";
+import Split from "react-split";
+import Playground from "./Playground/Playground";
+
+type WorkspaceProps = {};
+
+const Workspace: React.FC<WorkspaceProps> = () => {
+  return (
+    <Split className="split" minSize={0}>
+      <ProblemDescription />
+      <Playground />
+    </Split>
+  );
+};
+export default Workspace;
